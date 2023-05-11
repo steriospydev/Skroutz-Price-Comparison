f8132ddb237cecd3c985563b45c5df645a60f4d3
sterios <steriospydev@gmail.com>
2023-05-11 23:26:53 +0300

    Intitial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from settings import STORE_DIR, LATEST_FILE, URLS, LOG_DIR
+from utils import (extract_multiple_info,
+                   fetch_old_values,
+                   compare_prices)
+
+new_info = extract_multiple_info(multiple_urls=URLS)
+
+old_data = fetch_old_values(LOG_DIR)
+
+compare_prices(jsonfile=LATEST_FILE, dir_name=STORE_DIR,
+               new_data=new_info, past_data=old_data)
